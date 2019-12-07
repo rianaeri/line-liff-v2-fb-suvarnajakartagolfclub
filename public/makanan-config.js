79188b9bdf6a1f13055a3ce48873b45024fbe4b0
eri riana <rianaeri@gmail.com>
2019-12-07 22:44:00 +0700

    Create makanan-config.js

---
+++ public/makanan-config.js
@@ -0,0 +1,207 @@
+function loadMakanan() {
+    if (localStorage.list_data && localStorage.id_data) {
+        list_data = JSON.parse(localStorage.getItem('list_data'));
+        var data_app = "";
+        if (list_data.length > 0) {
+            data_app = '<table class="table table-striped table-dark">';
+            data_app += '<thead>' +
+                '<th>ID</th>' +
+                '<th>Nama</th>' +
+                '<th>Tanggal</th>' +
+                '<th>Makanan</th>' +
+                '<th>Hapus Makanan</th>' +
+                '<th>Lihat Makanan</th>' +
+                '<th>Edit Makanan</th>' +
+                '</thead> <tbody>';
+
+            for (i in list_data) {
+                data_app += '<tr>';
+                data_app +=
+                    '<td>' + list_data[i].id_data + ' </td>' +
+                    '<td>' + list_data[i].nama + ' </td>' +
+                    '<td>' + list_data[i].tanggal + ' </td>' +
+                    '<td>' + list_data[i].makanan + ' </td>' +
+                    '<td><a class="btn btn-danger btn-small" href="javascript:void(0)" onclick="hapusData(\'' + list_data[i].id_data + '\')">Hapus</a></td>' +
+                    '<td><a class="btn btn-danger btn-small" href="javascript:void(0)" onclick="lihatData(\'' + list_data[i].id_data + '\')">Lihat</a></td>' +
+                    '<td><a class="btn btn-warning btn-small" href="javascript:void(0)" onclick="editData(\'' + list_data[i].id_data + '\')">Edit</a></td>';
+                data_app += '</tr>';
+            }
+
+            data_app += '</tbody></table>';
+
+        }
+        else {
+            data_app = "Makanan masih kosong ...";
+        }
+
+
+        $('#list-makanan').html(data_app);
+        $('#list-makanan').hide();
+        $('#list-makanan').fadeIn(100);
+    }
+}
+
+function editData(id) {
+
+    if (localStorage.list_data && localStorage.id_data) {
+        list_data = JSON.parse(localStorage.getItem('list_data'));
+        idx_data = 0;
+        for (i in list_data) {
+            if (list_data[i].id_data == id) {
+                $("#eid_data").val(list_data[i].id_data);
+                $("#enama").val(list_data[i].nama);
+                $("#etanggal").val(list_data[i].tanggal);
+                $("#emakanan").val(list_data[i].makanan);
+                list_data.splice(idx_data, 1);
+            }
+            idx_data++;
+        }
+        gantiMenu('edit-data');
+
+    }
+
+}
+
+function lihatData(id) {
+    if (localStorage.list_data && localStorage.id_data) {
+        list_data = JSON.parse(localStorage.getItem('list_data'));
+        idx_data = 0;
+        for (i in list_data) {
+            if (list_data[i].id_data == id) {
+                $("#lid_data").val(list_data[i].id_data);
+                $("#lnama").val(list_data[i].nama);
+                $("#ltanggal").val(list_data[i].tanggal);
+                $("#lmakanan").val(list_data[i].makanan);
+                list_data.splice(idx_data, 1);
+            }
+            idx_data++;
+        }
+        gantiMenu('lihat-data');
+
+    }
+}
+
+
+function simpanData() {
+
+    if (!liff.isInClient()) {
+        sendAlertIfNotInClient();
+    } else {
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Pesanan Makanan baru berhasil disimpan"
+        }]).then(function() {
+            alert('Makanan Tersimpan');
+        }).catch(function(error) {
+            alert('Error ...');
+        });
+    }
+
+    nama = $('#nama').val();
+    tanggal = $('#tanggal').val();
+    agenda = $('#makanan').val();
+
+    if (localStorage.list_data && localStorage.id_data) {
+        list_data = JSON.parse(localStorage.getItem('list_data'));
+        id_data = parseInt(localStorage.getItem('id_data'));
+    }
+    else {
+        list_data = [];
+        id_data = 0;
+    }
+
+    id_data++;
+    list_data.push({ 'id_data': id_data, 'nama': nama, 'tanggal': tanggal, 'makanan': makanan });
+    localStorage.setItem('list_data', JSON.stringify(list_data));
+    localStorage.setItem('id_data', id_data);
+    document.getElementById('form-data').reset();
+    gantiMenu('list-makanan');
+
+    return false;
+}
+
+function simpanEditData() {
+
+    if (!liff.isInClient()) {
+        sendAlertIfNotInClient();
+    } else {
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Pesanan Makanan yang diedit sudah tersimpan"
+        }]).then(function() {
+            alert('Makanan tersimpan');
+        }).catch(function(error) {
+            alert('Error ...');
+        });
+    }
+
+    id_data = $('#eid_data').val();
+    nama = $('#enama').val();
+    tanggal = $('#etanggal').val();
+    agenda = $('#emakanan').val();
+
+    list_data.push({ 'id_data': id_data, 'nama': nama, 'tanggal': tanggal, 'makanan': makanan });
+    localStorage.setItem('list_data', JSON.stringify(list_data));
+    document.getElementById('eform-data').reset();
+    gantiMenu('list-makanan');
+
+    return false;
+}
+
+function hapusData(id) {
+
+    if (!liff.isInClient()) {
+        sendAlertIfNotInClient();
+    } else {
+        liff.sendMessages([{
+            'type': 'text',
+            'text': "Pesanan Makanan sudah terhapus"
+        }]).then(function() {
+            alert('Makanan sudah dihapus');
+        }).catch(function(error) {
+            alert('Error');
+        });
+    }
+
+    if (localStorage.list_data && localStorage.id_data) {
+        list_data = JSON.parse(localStorage.getItem('list_data'));
+
+        idx_data = 0;
+        for (i in list_data) {
+            if (list_data[i].id_data == id) {
+                list_data.splice(idx_data, 1);
+            }
+            idx_data++;
+        }
+
+        localStorage.setItem('list_data', JSON.stringify(list_data));
+        loadCatatan();
+    }
+}
+
+
+function gantiMenu(menu) {
+    if (menu == "list-makanan") {
+        loadCatatan();
+        $('#tambah-makanan').hide();
+        $('#list-makanan').fadeIn();
+        $('#edit-data').hide();
+        $('#lihat-data').hide();
+    }
+    else if (menu == "tambah-makanan") {
+        $('#tambah-makanan').fadeIn();
+        $('#list-makanan').hide();
+        $('#edit-data').hide();
+        $('#lihat-data').hide();
+    } else if (menu == "edit-data") {
+        $('#edit-data').fadeIn();
+        $('#tambah-makanan').hide();
+        $('#list-makanan').hide();
+        $('#lihat-data').hide();
+    } else if (menu == "lihat-data") {
+        $('#lihat-data').fadeIn();
+        $('#edit-data').hide();
+        $('#tambah-makanan').hide();
+        $('#list-makanan').hide();
+    }
+}
